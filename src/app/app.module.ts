84f880c72cfe0d76781ecb4a6df5e1cd4450a812
oussou-dev <oussou.dev@gmail.com>
2017-08-27 23:20:22 +0200

    Creating the Add Shopping Page & Styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AddShoppingPage } from './../pages/add-shopping/add-shopping';
 import { FIREBASE_CREDENTIALS } from './firebase.credentials';
 import {AngularFireModule} from 'angularfire2';
 
@@ -14,7 +15,8 @@ import { ShoppingListPage } from '../pages/shopping-list/shopping-list';
 @NgModule({
   declarations: [
     MyApp,
-    ShoppingListPage
+    ShoppingListPage,
+    AddShoppingPage
   ],
   imports: [
     BrowserModule,
@@ -25,7 +27,8 @@ import { ShoppingListPage } from '../pages/shopping-list/shopping-list';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    ShoppingListPage
+    ShoppingListPage,
+    AddShoppingPage
   ],
   providers: [
     StatusBar,
